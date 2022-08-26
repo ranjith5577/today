d5a67b3e7ad3cf1e441d9e5938698da9f25cea89
ranjith5577 <ranjithnataraj24@gmail.com>
2022-08-26 18:45:57 +0530

    asd

---
+++ src/test/java/org/nn/Git.java
@@ -0,0 +1,18 @@
+package org.nn;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Git {
+
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("sd");
+	}
+}
